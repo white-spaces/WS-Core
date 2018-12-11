46e2c38bd79aff60c214764aa64c4cc50166af78
white-spaces <white-spaces@outlook.com>
2018-12-11 20:39:09 +0000

    Initial Commit
    Files and folders created. Less, Component hierarchies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './Modules/Components/navigation/navigation.component';
+import { HomeComponent } from './Modules/Components/ws-core-view/home/home.component';
+import { DashComponent } from './Modules/Components/ws-core-view/dash/dash.component';
+import { EditorComponent } from './Modules/Components/ws-core-view/editor/editor/editor.component';
+import { SettingsComponent } from './Modules/Components/ws-core-view/settings/settings.component';
+import { TeamsComponent } from './Modules/Components/ws-core-view/teams/teams.component';
+import { ProfileComponent } from './Modules/Components/ws-core-view/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    DashComponent,
+    EditorComponent,
+    SettingsComponent,
+    TeamsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
