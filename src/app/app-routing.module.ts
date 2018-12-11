cee8ae7d49a75fff37925f8a791921fff7cdca64
white-spaces <white-spaces@outlook.com>
2018-12-11 21:31:23 +0000

    Second Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,64 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashComponent } from './Modules/Components/ws-core-view/dash/dash.component';
+import { EditorComponent } from './Modules/Components/ws-core-view/editor/editor.component';
+import { HomeComponent } from './Modules/Components/ws-core-view/home/home.component';
+import { ProfileComponent } from './Modules/Components/ws-core-view/profile/profile.component';
+import { SettingsComponent } from './Modules/Components/ws-core-view/settings/settings.component';
+import { TeamsComponent } from './Modules/Components/ws-core-view/teams/teams.component';
+import { PageNotFoundComponent } from './Modules/Components/page-not-found/page-not-found.component';
+import { LoginComponent } from './Modules/Components/login/login.component';
 
-const routes: Routes = [];
+
+const Path = "ws/"
+
+
+const routes: Routes = [
+  {
+    path: 'ws',
+    component: LoginComponent,
+    data: {title: 'White Spaces'}
+  },
+
+  // Dashboard routing
+  { path: Path + 'dash', 
+    component: DashComponent,
+    data: {title: 'Dashboard'}},
+  
+  // Editor routing  
+  { 
+    path: Path + 'editor', 
+    component: EditorComponent,
+    data: {title: 'Editor'}},
+  
+  // Home routing
+  { 
+    path: Path + 'home', 
+    component: HomeComponent,
+    data: {title: 'Home'}},
+  
+  // Profile routing
+  { 
+    path: Path + 'profile', 
+    component: ProfileComponent,
+    data: {title: 'Profile'}},
+  
+  // Settings routing
+  { 
+    path: Path + 'settings', 
+    component: SettingsComponent,
+    data: {title: 'Settings'}},
+  
+  // Teams routing
+  { 
+    path: Path + 'teams', 
+    component: TeamsComponent,
+    data: {title: 'Teams'}},
+
+  {
+    path: '**', component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './Modules/Components/navigation/navigation.component';
-import { HomeComponent } from './Modules/Components/ws-core-view/home/home.component';
-import { DashComponent } from './Modules/Components/ws-core-view/dash/dash.component';
-import { EditorComponent } from './Modules/Components/ws-core-view/editor/editor/editor.component';
-import { SettingsComponent } from './Modules/Components/ws-core-view/settings/settings.component';
-import { TeamsComponent } from './Modules/Components/ws-core-view/teams/teams.component';
-import { ProfileComponent } from './Modules/Components/ws-core-view/profile/profile.component';
+import { AppRoutingModule } from './Ws-Core/app-routing.module';
+import { AppComponent } from './Ws-Core/app.component';
+import { NavigationComponent } from './Ws-Core/Modules/Components/navigation/navigation.component';
+import { HomeComponent } from './Ws-Core/Modules/Components/ws-core-view/home/home.component';
+import { DashComponent } from './Ws-Core/Modules/Components/ws-core-view/dash/dash.component';
+import { EditorComponent } from './Ws-Core/Modules/Components/ws-core-view/editor/editor.component';
+import { SettingsComponent } from './Ws-Core/Modules/Components/ws-core-view/settings/settings.component';
+import { TeamsComponent } from './Ws-Core/Modules/Components/ws-core-view/teams/teams.component';
+import { ProfileComponent } from './Ws-Core/Modules/Components/ws-core-view/profile/profile.component';
+import { PageNotFoundComponent } from './Ws-Core/Modules/Components/page-not-found/page-not-found.component';
+import { LoginComponent } from './Ws-Core/Modules/Components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { ProfileComponent } from './Modules/Components/ws-core-view/profile/prof
     EditorComponent,
     SettingsComponent,
     TeamsComponent,
-    ProfileComponent
+    ProfileComponent,
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
